fb91bb6582337a4bdc0e2c0038c5a32d2a146523
Matías David Schwerdt <schwerdt.matias@gmail.com>
2023-03-15 17:11:59 -0300

    Python code

---
+++ main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+Esto es parte de la actividad sobre pipeline del dia jueves
+"""
+import os.path
+
+
+def main():
+    """
+    Si no existe, crea la carpeta build y el archivo index.html
+    """
+    folder = 'build/'
+    file = folder + 'index.html'
+
+    if os.path.exists(folder):
+        print('\nNo se pudo crear la carpeta nueva, ya existia')
+    else:
+        os.mkdir(folder)
+        print('\nCarpeta creada exitosamente.')
+
+    if os.path.isfile(file):
+        print('No se pudo crear el archivo nuevo, ya existia\n')
+    else:
+        with open(file, 'w', encoding="utf-8") as my_file:
+            my_file.write('Archivo creado en EATI 2023 por el' +
+                          'alumno Matias David Schwerdt.\n')
+            print("Archivo creado exitosamente")
+
+
+if __name__ == '__main__':
+    main()
